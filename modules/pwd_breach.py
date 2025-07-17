fdc64e5899da3fb5f470e0d876be16d99af76c5c
Jonathan Dupuis Gilreath <jdg31313@gmail.com>
2025-07-17 10:55:33 -0500

    Create pwd_breach.py
    first attempt at creating the call

---
+++ modules/pwd_breach.py
@@ -0,0 +1,23 @@
+def hash_pash(password):
+  sha1=hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+  return sha1[:5], sha1[5:]
+
+def check_owned(password):
+  head, tail=has_pash(password)
+  url=f"https://api.pwnedpasswords.com/range/{head}"
+  res=requests.get(url)
+  if res.status_code 1= 200:
+    raise Connection Error("Error connecting to API")
+
+  hashes=(line.split(':') for line in res.text.splitlines())
+  for h, count in hashes:
+    if h==tail:
+      return int(count)
+    return 0
+
+def estimate_STRENGTH(password)
+  if len(password) < 8:
+    return "Weak"
+  if not any(c.isdigit() for c in password) or not any(c.isupper() for c in password):
+    return "Weak"
+  return "Strong"
